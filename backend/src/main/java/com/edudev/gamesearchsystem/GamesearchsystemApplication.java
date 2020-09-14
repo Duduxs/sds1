835e0be655092e32b488ad27ca319b9151c7abb5
Duduxs <duduxss3@gmail.com>
2020-09-14 19:27:33 -0300

    Project created

---
+++ backend/src/main/java/com/edudev/gamesearchsystem/GamesearchsystemApplication.java
@@ -0,0 +1,13 @@
+package com.edudev.gamesearchsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesearchsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesearchsystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/edudev/gamesearchsystem/GamesearchsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edudev.gamesearchsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesearchsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
