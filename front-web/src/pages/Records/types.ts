5c2957332e291a936cbe5c83f0033f9a57994c5e
Duduxs <duduxss3@gmail.com>
2020-09-19 10:20:30 -0300

    Criando pagina de listagem dos registros

---
+++ front-web/src/pages/Records/types.ts
@@ -0,0 +1,16 @@
+export type RecordResponse = {
+    content: RecordItem[];
+    totalPages: number;
+}
+
+export type RecordItem = {
+    id: number,
+    moment: string,
+    name: string,
+    age: number,
+    gameTitle: string,
+    gamePlatform: Platform,
+    genreName: string
+}
+
+export type Platform = 'XBOX' | 'PC' | 'PLAYSTATION'; 
